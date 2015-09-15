6ba92ae850fbce3b2fba7d2da3764f2d31443426
tranch <tranch.xiao@gmail.com>
2015-09-16 00:23:18 +0800

    对查询结果处理函数以及编辑器快捷键进行了改进
    * 改进了 odbc_result_all 函数，添加了空数据时的处理
    * 该进了 Query Editor 的快捷键监听方式

--- config.php
+++ config.php
@@ -6,33 +6,37 @@ return [
         'password' => ''
     ],
     'util' => [
-        'odbc_result_all' => function ($result_resource, $tableFeatures="") {
-              $noFields = odbc_num_fields($result_resource);
+        'odbc_result_all' => function ($resultResource, $tableFeatures="") {
+            $noFields = odbc_num_fields($resultResource);
+            
+            $table = "<table $tableFeatures>" . PHP_EOL;
+            $table .= "<thead>" . PHP_EOL;
+            $table .= "<tr>" . PHP_EOL;
+            
+            for ($i = 1; $i <= $noFields; $i++) {
+                $field = odbc_field_name($resultResource, $i);
+                $table .= "<th>$field</th>" . PHP_EOL;
+            }
+            
+            $table .= '</tr>' . PHP_EOL;
+            $table .= '</thead>' . PHP_EOL;
+            
+            $table .= '<tbody>' . PHP_EOL;
+            $tableWithoutData = $table;
+            while ($r = odbc_fetch_array($resultResource)) {
+                $table .= "<tr>" . PHP_EOL;
+                foreach ($r as $kolonne) {
+                    $table .= "<td>$kolonne</td>" . PHP_EOL;
+                }
+                $table .= "</tr>" . PHP_EOL;
+            }
+            if ($table == $tableWithoutData) {
+                $table .= sprintf("<tr><td colspan='%d'>%s</td></tr>", $noFields, _('No data result.'));
+            }
+            $table .= '</tbody>' . PHP_EOL;
+            $table .= "</table>" . PHP_EOL;
               
-              $table = "<table $tableFeatures>" . PHP_EOL;
-              $table .= "<thead>" . PHP_EOL;
-              $table .= "<tr>" . PHP_EOL;
-              
-              for ($i = 1; $i <= $noFields; $i++) {
-                  $field = odbc_field_name($result_resource, $i);
-                  $table .= "<th>$field</th>" . PHP_EOL;
-              }
-              
-              $table .= '</tr>' . PHP_EOL;
-              $table .= '</thead>' . PHP_EOL;
-              
-              $table .= '<tbody>' . PHP_EOL;
-              while ($r = odbc_fetch_array($result_resource)) {
-                  $table .= "<tr>" . PHP_EOL;
-                  foreach ($r as $kolonne) {
-                      $table .= "<td>$kolonne</td>" . PHP_EOL;
-                  }
-                  $table .= "</tr>" . PHP_EOL;
-              }
-              $table .= '</tbody>' . PHP_EOL;
-              $table .= "</table>" . PHP_EOL;
-              
-              return $table;
+            return $table;
         }
     ],
     'language' => 'zh_CN'
