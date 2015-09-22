0d54f84cbba205d6e57783e433f163bf827ea815
tranch <tranch.xiao@gmail.com>
2015-09-23 02:27:06 +0800

    Fix bugs.
    * Fixed no result warning.
    * Added records count for result table.

--- config.php
+++ config.php
@@ -8,34 +8,49 @@ return [
     'util' => [
         'odbc_result_all' => function ($resultResource, $tableFeatures="") {
             $noFields = odbc_num_fields($resultResource);
-            
+            $noRows = 0;
+            $noAffectedRows = odbc_num_rows($resultResource);
             $table = "<table $tableFeatures>" . PHP_EOL;
+            
             $table .= "<thead>" . PHP_EOL;
             $table .= "<tr>" . PHP_EOL;
             
-            for ($i = 1; $i <= $noFields; $i++) {
-                $field = odbc_field_name($resultResource, $i);
-                $table .= "<th>$field</th>" . PHP_EOL;
+            if ($noFields > 0) {
+                for ($i = 1; $i <= $noFields; $i++) {
+                    $field = odbc_field_name($resultResource, $i);
+                    $table .= "<th>$field</th>" . PHP_EOL;
+                }
+            } else {
+                $table .= sprintf("<th>%s</th>\n", _('Result'));
             }
             
             $table .= '</tr>' . PHP_EOL;
             $table .= '</thead>' . PHP_EOL;
             
             $table .= '<tbody>' . PHP_EOL;
-            $tableWithoutData = $table;
-            while ($r = odbc_fetch_array($resultResource)) {
+            
+            while ($r = @odbc_fetch_array($resultResource)) {
                 $table .= "<tr>" . PHP_EOL;
                 foreach ($r as $kolonne) {
                     $table .= "<td>$kolonne</td>" . PHP_EOL;
                 }
+                $noRows ++;
                 $table .= "</tr>" . PHP_EOL;
             }
-            if ($table == $tableWithoutData) {
-                $table .= sprintf("<tr><td colspan='%d'>%s</td></tr>", $noFields, _('No data result.'));
+            
+            if ($noRows == 0) {
+                $table .= sprintf("<tr><td colspan='%d'>%s</td></tr>\n", $noFields, _('No data result.'));
+            } else {
+                $table .= '<tfoot>' . PHP_EOL;
+                $table .= '<tr>' . PHP_EOL;
+                $table .= sprintf("<th colspan='%d'> %s: %d</th>\n", $noFields, _('Number of rows'), $noRows);
+            
+                $table .= '</tr>' . PHP_EOL;
+                $table .= '</tfoot>' . PHP_EOL;
             }
             $table .= '</tbody>' . PHP_EOL;
             $table .= "</table>" . PHP_EOL;
-              
+            
             return $table;
         }
     ],
